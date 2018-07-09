c69225ad0cef54db5e9b7f4e8802df7822f850f7
kulikr <kulikr@post.bgu.ac.il>
2018-07-09 11:36:23 +0300

    sdsad

---
+++ MnistRun.py
@@ -0,0 +1,44 @@
+import keras
+from keras.datasets import mnist
+from keras.utils import plot_model
+import Main
+
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train /= 255
+x_test /= 255
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Main.squeeze_net(num_classes,(img_rows,img_cols,1))
+plot_model(model,'model.png')
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
