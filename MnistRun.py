79582d958dfdc23b98cafd64ca79df1b187dd494
kulikr <kulikr@post.bgu.ac.il>
2018-07-09 16:19:19 +0300

    push to keren

--- MnistRun.py
+++ MnistRun.py
@@ -1,17 +1,20 @@
 import keras
-from keras.datasets import mnist
+from keras.datasets import mnist,cifar10
 from keras.utils import plot_model
 import Main
 
 
 # input image dimensions
 img_rows, img_cols = 28, 28
-batch_size = 128
+batch_size = 256
 num_classes = 10
-epochs = 12
+epochs = 1
+
+tbCallBack = keras.callbacks.TensorBoard(log_dir='./Graph', histogram_freq=0,
+          write_graph=True, write_images=False)
 
 # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
 x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
@@ -27,8 +30,8 @@ x_test /= 255
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
-model = Main.squeeze_net(num_classes,(img_rows,img_cols,1))
-plot_model(model,'model.png')
+model = Main.squeeze_net(num_classes,(img_rows,img_cols,3))
+# plot_model(model,'model.png')
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
@@ -38,7 +41,7 @@ model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
-          validation_data=(x_test, y_test))
+          validation_data=(x_test, y_test), callbacks=[tbCallBack])
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
